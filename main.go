ef01c74270a10d198540101216fccc6a09402e6a
Outsider <outsideris@gmail.com>
2018-04-08 18:16:01 +0900

    add health check

--- main.go
+++ main.go
@@ -3,15 +3,16 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/k8s-study/user-service/db"
+	"github.com/k8s-study/user-service/controllers"
 )
 
 func main() {
 	db.Init()
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/health", controllers.Health)
+	}
+	r.Run()
 }
